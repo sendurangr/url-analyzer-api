695504c35bebe0c80cfa8bfc98c42e823b277e72
Senduran <senduran40@gmail.com>
2025-04-29 01:11:03 +0530

    Implement health check logging and add UrlAnalyzerHandler stub

--- internal/handler/healthcheck_handler.go
+++ internal/handler/healthcheck_handler.go
@@ -4,9 +4,12 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+
+	"log/slog"
 )
 
 func HealthCheckHandler(context *gin.Context) {
+	slog.Info("healthck is called")
 	context.JSON(http.StatusOK, gin.H{
 		"message": "Server is running successfully!!",
 	})
